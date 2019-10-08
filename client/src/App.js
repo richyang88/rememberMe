25041f4ba67e2fe5b048d909acdf2db8f8d335ee
Richard Yang <rich.yang88@gmail.com>
2019-10-08 18:28:18 -0400

    getting users from server

--- client/src/App.js
+++ client/src/App.js
@@ -3,42 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  
+
   state = {
     users: [
       {
-        username:'',
-        email:''
+        username: '',
+        email: ''
       }
     ]
   }
 
-  componentDidMount = () =>{
+  //getting user info from database
+  componentDidMount = () => {
     fetch('/api/user/')
       .then(res => res.json())
-      .then(users=> this.setState({ users }))
+      .then(users => this.setState({ users }))
       .catch(err => {
         console.log(err)
       })
   }
 
+  //
+  getUsersFromServer = () =>
+    fetch('/api/user/')
+      .then(res => {
+        res.json()
+        console.log(res)
+      })
+
+  renderUserDropdown = () => {
+    return (
+      <select>
+      {/* map users into new array user, then get new array elem id */}
+        {this.state.users.map(user =>
+          <option value={user.id}>
+            {users.username}
+          </option>)}
+      </select>
+    )
+  }
+
   render = () => {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+        <header>Remember Me</header>
+        <p></p>
       </div>
     );
   }
