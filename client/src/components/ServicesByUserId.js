1464d6b0124e2f9edefa0951b56c8858fcca7a0d
Richard Yang <rich.yang88@gmail.com>
2019-10-10 15:23:40 -0400

    links working

--- client/src/components/ServicesByUserId.js
+++ client/src/components/ServicesByUserId.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, Route, Switch } from 'react-router-dom';
 
 export default class ServicesByUserId extends React.Component {
     state = {
@@ -9,26 +10,44 @@ export default class ServicesByUserId extends React.Component {
         ]
     }
 
+    componentDidMount = () => {
+        fetch('/api/service/')
+            .then(res => res.json())
+            .then(services => this.setState({ services }))
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    //make this into another component to be imported, call using router
+    getUsersFromServer = () =>
+        fetch('/api/service/')
+            .then(res => {
+                res.json()
+                console.log(res)
+            })
+
     renderServiceDropdown = () => {
         return (
-          <ol>
-          {/* map users into new array user, then get new array elem id */}
-            {this.state.services.map(service =>
-              <li value={service.id}>
-              
-                {service.serviceName}
-                {'here'}
-              </li>)}
-          </ol>
+            <ol>
+                {/* map users into new array user, then get new array elem id */}
+                {this.state.services.map(service =>
+                    <li value={service.id}>
+                        <Link to={`/password/${service.id}`}>
+                        {service.serviceName}
+                        {'here'}
+                        </Link>
+                    </li>)}
+            </ol>
         )
-      }
+    }
 
     render = () => {
         return (
-        <div className="App">
-            <header>services</header>
-            <p>{this.renderServiceDropdown()}</p>
-        </div>
+            <div className="App">
+                <header>services</header>
+                <p>{this.renderServiceDropdown()}</p>
+            </div>
         )
 
     }
