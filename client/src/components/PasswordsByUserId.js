7ee2e8d114dfe5ed538b4926df2eb648bb655f9f
Richard Yang <rich.yang88@gmail.com>
2019-10-11 11:10:47 -0400

    adding password component

---
+++ client/src/components/PasswordsByUserId.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Link, Route, Switch } from 'react-router-dom';
+
+export default class PasswordByUserId extends React.Component {
+    state = {
+        services: [
+            {
+                serviceName: "",
+            }
+        ]
+    }
+
+    componentDidMount = () => {
+        fetch('/api/service/')
+            .then(res => res.json())
+            .then(services => this.setState({ services }))
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    //make this into another component to be imported, call using router
+    getUsersFromServer = () =>
+        fetch('/api/service/')
+            .then(res => {
+                res.json()
+                console.log(res)
+            })
+
+    renderServiceDropdown = () => {
+        return (
+            <ol>
+                {/* map users into new array user, then get new array elem id */}
+                {this.state.services.map(service =>
+                    <li value={service.id}>
+                        <Link to={`/password/${service.id}`}>
+                        {service.serviceName}
+                        {'here'}
+                        </Link>
+                    </li>)}
+            </ol>
+        )
+    }
+
+    render = () => {
+        return (
+            <div className="App">
+                <header>services</header>
+                <p>{this.renderServiceDropdown()}</p>
+            </div>
+        )
+
+    }
+}
+
+// export ServicesByUserId
